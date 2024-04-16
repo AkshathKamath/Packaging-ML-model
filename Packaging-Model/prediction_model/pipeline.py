a4d3b0b9890aa7e303da286979b91af604532781
AkshathKamath <akshathkamathwork@gmail.com>
2024-04-16 13:34:24 +0530

    Created Pipeline

--- Packaging-Model/prediction_model/pipeline.py
+++ Packaging-Model/prediction_model/pipeline.py
@@ -4,3 +4,16 @@ from sklearn.linear_model import LogisticRegression
 import prediction_model.processing.pre_processing as pp
 from prediction_model.config import config
 
+classification_pipeline = Pipeline(
+    [
+        ('MedianImputation', pp.MedianImputer(variables = config.NUM_FEATURES)),
+        ('ModeImputation',pp.ModeImputer(variables = config.CAT_FEATURES)),
+        ('DropFeatures', pp.DropColumns(variables = config.DROP_FEATURES)),
+        ('AddColumns', pp.AddColumns(col1 = config.FEATURE_TO_MODIFY, col2 = config.FEATURE_TO_ADD )),
+        ('LabelEncoder',pp.LabelEncoder(variables = config.FEATURES_TO_ENCODE)),
+        ('LogTransform',pp.LogTransformer(variables = config.LOG_FEATURES)),
+        ('MinMaxScale', MinMaxScaler()),
+        ('LogisticClassifier',LogisticRegression(random_state=0))
+
+    ]
+)
