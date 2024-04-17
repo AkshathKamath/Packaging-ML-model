31c3222e2d739371149408ea30606c2e28deed6d
AkshathKamath <akshathkamathwork@gmail.com>
2024-04-17 11:39:51 +0530

    Mean -> Median Imputer

--- Packaging-Model/prediction_model/pipeline.py
+++ Packaging-Model/prediction_model/pipeline.py
@@ -9,7 +9,7 @@ classification_pipeline = Pipeline(
     [
         # ('DomainProcessing',pp.DomainProcessing(variable_to_modify = config.FEATURE_TO_MODIFY,
         # variable_to_add = config.FEATURE_TO_ADD)),
-        ('MeanImputation', pp.MedianImputer(variables=config.NUM_FEATURES)),
+        ('MedianImputation', pp.MedianImputer(variables=config.NUM_FEATURES)),
         ('ModeImputation',pp.ModeImputer(variables=config.CAT_FEATURES)),
         ('DropFeatures', pp.DropColumns(variables_to_drop=config.DROP_FEATURES)),
         ('LabelEncoder',pp.CustomLabelEncoder(variables=config.FEATURES_TO_ENCODE)),
