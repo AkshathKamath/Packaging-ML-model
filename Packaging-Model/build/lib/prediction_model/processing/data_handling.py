9c52497a54cb13ab68a03dd20e2f24c935b66df1
AkshathKamath <akshathkamathwork@gmail.com>
2024-04-30 16:56:26 +0530

    Created Package

---
+++ Packaging-Model/build/lib/prediction_model/processing/data_handling.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+import joblib
+from prediction_model.config import config
+
+def load_dataset(data_file_name): ## To load the datsets we will pass config.TRAIN_FILE as param for train.csv
+    filepath = os.path.join(config.DATAPATH, data_file_name)
+    _data_ = pd.read_csv(filepath) ## Read the csv file as dataframe from the filepath
+    return _data_ ## returns dataframe
+
+def save_pipeline(pipeline_to_save): ## Serialization to store our created model
+    save_path = os.path.join(config.SAVE_MODEL_PATH, config.MODEL_NAME)
+    joblib.dump(pipeline_to_save, save_path)
+    print("Model has been saved as ", config.MODEL_NAME)
+
+def load_pipeline(pipeline_to_load): ## Deserialization to load model
+    model_path = os.path.join(config.SAVE_MODEL_PATH,config.MODEL_NAME)
+    model_loaded = joblib.load(model_path)
+    print("Model has been loaded")
+    return model_loaded
